c8d17a91284c96b37d33b4ba1eeac677d7493bfa
zzzdks760 <zzzdks760@naver.com>
2023-10-26 15:32:46 +0900

    SWEA 1979

--- Java/SW1979.java
+++ Java/SW1979.java
@@ -2,7 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class SW1979 {
-    static int[][] map;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
@@ -12,18 +11,52 @@ public class SW1979 {
             int N = Integer.parseInt(st.nextToken());
             int K = Integer.parseInt(st.nextToken());
 
-            int result = 0;
-            map = new int[N][N];
+            int[][] graph = new int[N][N];
 
+            //graph 채우기
             for (int i = 0; i < N; i++) {
                 StringTokenizer st2 = new StringTokenizer(br.readLine());
                 for (int j = 0; j < N; j++) {
-                    map[i][j] = Integer.parseInt(st2.nextToken());
+                    graph[i][j] = Integer.parseInt(st2.nextToken());
 
                 }
             }
-            System.out.println(Arrays.deepToString(map));
+            
 
+            int answer = 0;
+            for (int x = 0; x < N; x++) {
+                int w_cnt = 0; int h_cnt = 0;
+                for (int y = 0; y < N; y++) {
+                    //가로
+                    if (graph[x][y] == 1) {
+                        w_cnt++;
+                    } else {
+                        w_cnt = 0;
+                    }
+                    
+                    if (w_cnt == K && y != N-1 && graph[x][y+1] == 0) {
+                        answer++;
+                    } else if (w_cnt == K && y == N-1) {
+                        answer++;
+                    }
+                    
+                    //세로
+                    if (graph[y][x] == 1) {
+                        h_cnt++;
+                    } else {
+                        h_cnt = 0;
+                    }
+
+                    if (h_cnt == K && y != N-1 && graph[y+1][x] != 1) {
+                        answer++;
+                    } else if (h_cnt == K && y == N-1) {
+                        answer++;
+                    }
+
+                }    
+            }
+
+            System.out.println("#" + t + " " + answer);
         }
     }
 }
