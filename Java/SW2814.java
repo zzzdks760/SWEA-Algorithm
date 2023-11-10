a6ad6cba373f8e7f09202dcd09fe1f51cbfdce5a
zzzdks760 <zzzdks760@naver.com>
2023-11-10 20:28:13 +0900

    SWEA 2814

---
+++ Java/SW2814.java
@@ -0,0 +1,48 @@
+import java.io.*;
+import java.util.*;
+
+public class SW2814 {
+    static StringBuilder sb = new StringBuilder();
+    static int[][] graph;
+    static boolean[] visited;
+    static int N, M, answer;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int t = 1; t <= T; t++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st.nextToken());
+            M = Integer.parseInt(st.nextToken());
+            graph = new int[N+1][N+1];
+            visited = new boolean[N+1];
+            answer = 0;
+
+            for (int i = 0; i < M; i++) {
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                graph[x][y] = graph[y][x] = 1; //정점 연결
+            }
+
+            for (int i = 1; i <= N; i++) {
+                dfs(i, 1);
+                visited[i] = false;
+            }
+            sb.append("#").append(t).append(" ").append(answer).append("\n");
+        }
+        System.out.println(sb);
+    }
+
+    static void dfs(int idx, int cnt) {
+
+        for (int i = 1; i <= N; i++) {
+            if (graph[idx][i] == 1 && !visited[i]) {
+                visited[i] = true;
+                dfs(i,cnt+1);
+                visited[i] = false;
+            }
+        }
+        answer = Math.max(answer, cnt);
+    }
+}
